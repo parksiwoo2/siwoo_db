b053483dd1b6d91353f0254c3ae79c4571f37a72
parksiwoo2 <132458293+parksiwoo2@users.noreply.github.com>
2025-03-30 10:08:04 +0000

    03/30

--- mydjango/myapp/models.py
+++ mydjango/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Singer(models.Model):
+    singer=models.CharField(max_length=10)
+
+class Song(models.Model):
+    rank=models.IntegerField()
+    album=models.CharField(max_length=50)
+    name=models.CharField(max_length=30)
+    singer=models.ForeignKey(Singer,on_delete=models.CASCADE)
+    cover_image=models.URLField(blank=True)
+    lyric=models.TextField()
+    genre=models.CharField(max_length=15)
+    date=models.DateField()
+    good=models.IntegerField()
--- mydjango/myapp/views.py
+++ mydjango/myapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from myapp.models import Song
+import json
 
-# Create your views here.
+def save_date(request:HttpRequest):
+    file_path='myapp/melon'
+    with open(file_path,'r',encoding='utf-8') as json_file:
+        data=json_file.load()
+    for item in data:
+        Song.objects.create(
+            rank=item["곡일련번호"]
+            album=item["앨범"]
+            name=item["곡명"]
+            singer=item["가수"]
+            cover_image=item["커버이미지_주소"]
+            lyric=item["가사"]
+            genre=item["장르"]
+            date=item["발매일"]
+            good=item["좋아요"]
+        )
+    
